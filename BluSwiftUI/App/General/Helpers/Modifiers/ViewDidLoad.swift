2f644a7709db8c0fd5eb916e0da08ea531da7df4
hosseinhajimirza <hosseinhajimirza77@gmail.com>
2023-05-15 22:01:07 +0330

    view modifiers added

---
+++ BluSwiftUI/App/General/Helpers/Modifiers/ViewDidLoad.swift
@@ -0,0 +1,29 @@
+//
+//  ViewDidLoad.swift
+//  BluSwiftUI
+//
+//  Created by Hossein Hajimirza on 5/15/23.
+//
+
+import SwiftUI
+
+struct ViewDidLoadModifier: ViewModifier {
+    @State private var viewDidLoad = false
+    let action: (() -> Void)?
+    
+    func body(content: Content) -> some View {
+        content
+            .onAppear {
+                if viewDidLoad == false {
+                    viewDidLoad = true
+                    action?()
+                }
+            }
+    }
+}
+
+extension View {
+    func onViewDidLoad(perform action: (() -> Void)? = nil) -> some View {
+        self.modifier(ViewDidLoadModifier(action: action))
+    }
+}
