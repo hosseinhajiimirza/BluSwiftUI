0bbe9e02c32c372612c671981c84b9491fb79f1f
hosseinhajimirza <hosseinhajimirza77@gmail.com>
2023-05-15 18:13:03 +0330

    Initial Commit

---
+++ BluSwiftUI/BluSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluSwiftUIApp.swift
+//  BluSwiftUI
+//
+//  Created by Hossein Hajimirza on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct BluSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
