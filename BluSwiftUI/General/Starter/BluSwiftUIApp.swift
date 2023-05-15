ab03742b289a2992ab64dae11b39d1eee3d30b46
hosseinhajimirza <hosseinhajimirza77@gmail.com>
2023-05-15 18:43:06 +0330

    appDelegate implemented and added to swiftUI main page

--- BluSwiftUI/General/Starter/BluSwiftUIApp.swift
+++ BluSwiftUI/General/Starter/BluSwiftUIApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct BluSwiftUIApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             Root()
