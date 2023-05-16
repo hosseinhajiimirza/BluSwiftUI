fe50f625b1f5df411ff019d857f65ba81d4d0af0
hosseinhajimirza <hosseinhajimirza77@gmail.com>
2023-05-16 15:39:28 +0330

    core data for transfer model added

---
+++ TransferCDModel+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  TransferCDModel+CoreDataClass.swift
+//  BluSwiftUI
+//
+//  Created by Hossein Hajimirza on 5/16/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TransferCDModel)
+public class TransferCDModel: NSManagedObject {
+
+}
