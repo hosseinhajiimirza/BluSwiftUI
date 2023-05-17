fd4cc1a38be2b57d681749a48d86bed8457c74a4
hosseinhajimirza <hosseinhajimirza77@gmail.com>
2023-05-17 04:26:00 +0330

    unit testing added for home api

--- BluSwiftUI_UnitTests/TabGroup/HomeGroup/Home/HomeUnitTest.swift
+++ BluSwiftUI_UnitTests/TabGroup/HomeGroup/Home/HomeUnitTest.swift
@@ -6,30 +6,51 @@
 //
 
 import XCTest
+@testable import BluSwiftUI
 
+@MainActor
 final class HomeUnitTest: XCTestCase {
+    let expectation = XCTestExpectation(description: #function)
+    var viewModel: HomeViewModel?
+    var homeMockAPI: HomeMockAPI?
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        homeMockAPI = .init()
+        viewModel = .init(homeAPIProtocol: homeMockAPI!)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        viewModel = nil
+        homeMockAPI = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_viewModel_getAllPosts_shouldDecodeModel() {
+        // Given
+        homeMockAPI?.mockNetworkViewModel.shouldReturnError = false
+        // When
+        Task {
+            await viewModel?.getTransferList()
+            // Then
+            XCTAssertNotEqual(viewModel?.transfers.count, 0)
+            self.expectation.fulfill()
         }
+        
+        wait(for: [expectation], timeout: 5.0)
     }
-
+    // Or:
+    func test_getAllPosts_shouldDecodeModel() {
+        // Given
+        homeMockAPI?.mockNetworkViewModel.shouldReturnError = false
+        // When
+        Task {
+            if let model = try? await homeMockAPI?.getTransferList() {
+                // Then
+                XCTAssertNotNil(model)
+                XCTAssertNotEqual(model.count, 0)
+                self.expectation.fulfill()
+            }
+        }
+        
+        wait(for: [expectation], timeout: 5.0)
+    }
 }
